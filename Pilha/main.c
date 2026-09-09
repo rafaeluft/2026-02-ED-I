f1824f803c0ad2f216c6bb42d14f32cb48213193
pprafaellima <pp.rafaellima@gmail.com>
2026-09-09 10:23:39 -0300

    Pilha Estatica

--- Pilha/main.c
+++ Pilha/main.c
@@ -9,8 +9,12 @@ int main(){
     Stack_push(piula, 3);
 
     int temp;
+    Stack_head(piula, &temp);
+    printf("O elemento do topo atual é: %d\n", temp);
+    //Aqui remove os elementos
     while(Stack_pop(piula, &temp))
-        printf("%d\n", temp);
-
+        printf("Valor removido: %d, Qtde atual na pilha: %u\n", temp, Stack_qty(piula));
+    
+    Stack_destroy(piula);
     return 0;
 }
--- Pilha/pilha_estatica.c
+++ Pilha/pilha_estatica.c
@@ -29,3 +29,22 @@ bool Stack_pop(Stack* pilha, int* pvalor){
         return true;
     } return false;
 }
+
+bool Stack_head(Stack* pilha, int* pvalor){
+    if(pilha!=NULL && pilha->qty>0){
+        //Sem decrementar a quantidade
+        //Só copiando o valor do topo
+        *pvalor = pilha->data[pilha->qty-1];
+        return true;
+    } return false;
+}
+
+void Stack_destroy(Stack* pilha){
+    free(pilha);
+}   
+
+unsigned int Stack_qty(Stack* pilha){
+    if(pilha!=NULL)
+        return pilha->qty;
+    return 0;
+}
